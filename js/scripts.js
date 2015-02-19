b66c9ef09591650e6fe8871b286fd43a3939cf43
Tina Ramsey <tina.ramsey87@gmail.com>
2015-02-18 21:08:59 -0800

    add support for multiple addresses

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,85 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var inputtedAddress = $("input#new-address").val();
-    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress };
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, addresses: [] };
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input#new-address").val("");
+  $(".new-address").each(function() {
+    var inputtedStreet = $(this).find("input.new-street").val();
+    var inputtedCity = $(this).find("input.new-city").val();
+    var inputtedState = $(this).find("input.new-state").val();
+
+    var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
+    newContact.addresses.push(newAddress);
+
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                               '</div>');
+
+  });
+
+
+  $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+
+      $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
-      $(".address").text(newContact.address);
+
+      $("ul#addresses").text("");
+
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
+      });
+    });
+
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+      $("input#new-street").val("");
+      $("input#new-city").val("");
+      $("input#new-state").val("");
     });
   });
 });
+
+
+
+
+// $(document).ready(function() {
+//   $("form#new-contact").submit(function(event) {
+//     event.preventDefault();
+//
+//     var inputtedFirstName = $("input#new-first-name").val();
+//     var inputtedLastName = $("input#new-last-name").val();
+//     var inputtedAddress = $("input#new-address").val();
+//     var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress };
+//
+//     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+//
+//     $("input#new-first-name").val("");
+//     $("input#new-last-name").val("");
+//     $("input#new-address").val("");
+//
+//     $(".contact").last().click(function() {
+//       $("#show-contact").show();
+//       $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+//       $(".first-name").text(newContact.firstName);
+//       $(".last-name").text(newContact.lastName);
+//       $(".address").text(newContact.address);
+//     });
+//   });
+// });
